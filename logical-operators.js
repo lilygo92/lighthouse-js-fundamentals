a9c4d7ba4ba72b43fa0f8605680e7e22f9c008df
Lily Go <lilygo7192@gmail.com>
2023-02-14 17:05:52 -0800

    Add more conditional examples

---
+++ logical-operators.js
@@ -0,0 +1,18 @@
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log('You are eligible to vote.');
+}
+
+temperature = 50;
+
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
